d428be55ff481593d217dcd5345eb16e23b668b4
Alexander Hultnér <ahultner@gmail.com>
2023-12-05 00:36:12 +0100

    feat: D4 solved

--- aoc/day_04/core.py
+++ aoc/day_04/core.py
@@ -1,16 +1,53 @@
 """
 Advent of Code - Day 04
 """
-from typing import Tuple
+from typing import NamedTuple, Iterator
+import re
 
 from aoc.day_04.seed import p1
 
 
-def part_1(puzzle_input: Tuple[int] = p1) -> int:
-    """"""
-    return 0
+class Card(NamedTuple):
+    id: int
+    card_numbers: set[int]
+    winning_numbers: set[int]
 
 
-def part_2(puzzle_input: Tuple[int] = p1) -> int:
+def parse(raw_str: str) -> Iterator[Card]:
+    # pattern = r"Card (P<id>\d+):(P<card>(\s+(\d+))+) \|(P<winning>(\s+(\d+))+)"
+    pattern = r"Card\s+(\d+):((\s+\d+)+) \|((\s+\d+)+)"
+    for line in raw_str.splitlines():
+        for match in re.findall(pattern, line):
+            # Just to make sure double winnings isn't possible
+            assert (
+                len(set(int(v) for v in match[3].split())) ==
+                len(list(int(v) for v in match[3].split()))
+            )
+            assert (
+                len(set(int(v) for v in match[1].split())) ==
+                len(list(int(v) for v in match[1].split()))
+            )
+            yield Card(
+                int(match[0]),
+                set(int(v) for v in match[1].split()),
+                set(int(v) for v in match[3].split()),
+            )
+
+
+def part_1(puzzle_input: str = p1) -> int:
     """"""
-    return 0
+    cards = parse(puzzle_input)
+    return sum(
+            2**(exp-1)
+            for card in cards
+            if (exp := len(card.winning_numbers & card.card_numbers)) > 0
+    )
+
+
+def part_2(puzzle_input: str = p1) -> int:
+    """"""
+    cards = list(parse(puzzle_input))
+    for card in cards:
+        winners = card.winning_numbers & card.card_numbers
+        cards += cards[card.id: card.id+len(winners)]
+    return len(cards)
--- tests/test_day_04.py
+++ tests/test_day_04.py
@@ -2,18 +2,27 @@ from typing import Any
 
 from aoc.day_04.core import part_1, part_2
 
-sample_seed_1 = (1,)
+sample_seed_1 = """
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+""".strip()
 
 answers = (
-    0,
-    0,
+    13,
+    30,
 )
 
 
 def test_parts() -> None:
     # Oracle says so
-    assert part_1() == 0
-    assert part_2() == 0
+    assert part_1() > 10231
+    assert part_1() == 25174
+    assert part_2() == 6420979
+    pass
 
 
 def verify_day(data: Any, expected_1: Any, expected_2: Any) -> None:
